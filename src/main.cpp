8d0d3fbb2ab01ea72a14a7f05e388e4d3e959a26
christinekemunto736 <christinekemunto736@gmail.com>
2026-02-06 23:39:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Book class
+class Book {
+public:
+    int id;
+    string title;
+    string author;
+    bool isBorrowed;
+
+    Book(int i, string t, string a) {
+        id = i;
+        title = t;
+        author = a;
+        isBorrowed = false;
+    }
+};
+
+// User class
+class User {
+public:
+    int userId;
+    string name;
+
+    User(int id, string n) {
+        userId = id;
+        name = n;
+    }
+};
+
+// Library class
+class Library {
+private:
+    vector<Book> books;
+
+public:
+    void addBook(Book b) {
+        books.push_back(b);
+    }
+
+    void displayBooks() {
+        for (Book b : books) {
+            cout << b.id << " | " << b.title << " | " << b.author;
+            if (b.isBorrowed)
+                cout << " | Borrowed";
+            else
+                cout << " | Available";
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Library lib;
+
+    lib.addBook(Book(1, "Clean Code", "Robert Martin"));
+    lib.addBook(Book(2, "C++ Primer", "Lippman"));
+
+    cout << "Library Books:" << endl;
+    lib.displayBooks();
+
+    return 0;
+}
 
